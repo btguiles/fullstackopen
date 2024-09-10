e5be781a9e639173a14df6a9e906e8800c828866
Ben Guiles <59574591+btguiles@users.noreply.github.com>
2024-09-09 21:34:33 -0700

    everything up until exercise 1.1

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,8 +1,21 @@
-const App = () => {
+const Hello = (props) => {
+  console.log(props)
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <>
+      <p>Hello, {props.name}, you are {props.age} years old. </p>
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'Ben'
+  const age = 37
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name={name} age={age} />
+      <Hello name='Arlo' age={12} />
+    </>
   )
 }
 
