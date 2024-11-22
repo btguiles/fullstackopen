6e1b589d8ef6a070539f75e0a40f987775174ff0
btguiles <btguiles@gmail.com>
2024-11-21 16:50:43 -0800

    Some state stuff

---
+++ README.md
@@ -0,0 +1,13 @@
+## Create a Vite project
+# npm 6.x (outdated, but still used by some):
+npm create vite@latest {project} --template react
+
+# npm 7+, extra double-dash is needed:
+npm create vite@latest {project} -- --template react
+
+cd {project}
+npm install
+
+## Test project locally
+1. cd {project}
+2. npm run dev
--- part1/state/src/App.jsx
+++ part1/state/src/App.jsx
@@ -1,20 +1,42 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Input = (props) => {
+  return (
+    <input value={props.value} onChange={props.onChange} />
+  )
+}
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
  
   const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
   const setToZero = () => setCounter(0)
+  const setToValue = (value) => setCounter(value)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text='Plus' />
+      <Button onClick={setToZero} text='Zero' />
+      <Button onClick={decreaseByOne} text='Minus' />
+      <Input value={counter} onChange={({ target }) => setToValue(target.value)} />
+      
+    
     </div>
   )
 }
